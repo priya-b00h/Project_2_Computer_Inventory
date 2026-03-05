f5ce43257479342059eb8f10ef42274e747a70a5
Ian Gui <ian.suyuan.gui@gmail.com>
2026-03-04 21:06:22 -0800

    modified:   inventory.py modified:   linked.py

--- inventory.py
+++ inventory.py
@@ -1,1 +1,57 @@
+from linked import LinkedComputer
 
+def mainMenu():
+    """
+    Prints the main menu and manages user input.
+    """
+
+    computers = LinkedComputer()
+
+    def addComputer():
+        """Adds computer with user inputted info to linked structure"""
+        ip = input("Enter the computer's IP address: ")
+        year = input("Enter the year of purchase: ")
+        os = input("Enter the operating system: ")
+        drive_capacity = input("Enter the C drive capacity: ")
+
+    def removeComputers():
+        amount = input("How many computers do you want to remove: ")
+
+        if amount <= computers.size:
+            if amount > 0:
+                print("Year purchased  IP address   Storage Space   Operating System")
+                print("-------------------------------------------------------------")
+
+            for _ in range(amount):
+                """removes """
+                removed_object = computers.remove()
+                print("2016".ljust(15), "1.1.1.1".ljust(15), "Filesystem, 999 GB".ljust(21), "Windows 11")
+
+    while True:
+        """prints main menu"""
+        print("")
+        print("------------------------------------")
+        print("Menu")
+        print("L. List all computers in your inventory")
+        print("")
+        print("A. Add a computer")
+        print("R. Remove some computers")
+        print("")
+        print("Q. Quit")
+        print("------------------------------------")
+        print("")
+
+        choice = input("Enter your choice: ").lower()
+        print("")
+
+        """checks user input"""
+        if choice == "l":
+            print(computers)
+        elif choice == "a":
+            addComputer()
+        elif choice == "r":
+            removeComputers
+        elif choice == "q":
+            break
+        else:
+            print("Invalid choice.")
--- linked.py
+++ linked.py
@@ -1,9 +1,15 @@
 class LinkedComputer:
     """Represents a linked structure that stores ComputerSystems."""
     def __init__(self):
+        """Initiates head to none, as there are no items yet"""
         self.head = None
+        self.size = 0
 
     def add(self, computerObject):
+        """This function adds a computerObject into the correct position"""
+
+        self.size += 1
+
         if self.head == None:
             self.head = computerObject
             computerObject.next = None
@@ -12,6 +18,7 @@ class LinkedComputer:
             self.head = computerObject
         else:
             current = self.head
+            """Moves through the linked structure until the new object is in the correct spot"""
             while current.next and computerObject.year >= current.next.year:
                 current = current.next
 
@@ -19,8 +26,17 @@ class LinkedComputer:
             current.next = computerObject
 
     def remove(self):
+        """Removes the first computer"""
         if self.head != None:
             removedValue = self.head
             self.head = removedValue.next
             removedValue.next = None
+            self.size -= 1
             return removedValue
+    
+    def __str__(self):
+        """This function prints the linked structure in a menu format."""
+
+        print("Year purchased  IP address      Storage Space         Operating System")
+        print("----------------------------------------------------------------------")
+        print("2016".ljust(15), "1.1.1.1".ljust(15), "Filesystem, 999 GB".ljust(21), "Windows 11")
